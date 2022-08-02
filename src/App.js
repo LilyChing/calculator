9eb212dbff8bdeca3f2c81d795450c77ae6528bd
Samuel <kamshing2360@gmail.com>
2022-08-03 00:19:01 +0800

    layout update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Screen} from './Screen';
-import {ButtonBox} from './ButtonBox';
-import {Button} from './Button';
+import { Screen } from './Screen';
+import { ButtonBox } from './ButtonBox';
+import { Button } from './Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { screenNum: 0 };
+  }
+
+  genButton() {
+    const btnValue = [
+      ['AC', '+-', '%', '/'],
+      [7, 8, 9, 'x'],
+      [4, 5, 6, '-'],
+      [1, 2, 3, '+'],
+      [0, '.', '=']
+    ];
+
+    const btnHtml = btnValue.flat().map((value, index) =>
+      <Button
+        className={index < 3 ? "button-light" : index == 3 || index == 7 || index == 11 || index == 15 ? "button-orange" : index == 16 ? "button-zero" : "button"}
+        onClick={() => {
+          console.log(value);
+        }}
+        value={(value)}
+        key={index}
+      />
+    );
+    return btnHtml;
+  }
+
+  render() {
+    return (
+      <div className='main'>
+        <Screen />
+        <ButtonBox>
+          {this.genButton()}
+        </ButtonBox>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/Screen.js
+++ src/Screen.js
@@ -3,4 +3,8 @@ export class Screen extends React.Component {
     constructor(props){
         super(props);
     }
+
+    render(){
+        return <h1 className='screen'>123</h1>
+    }
 }
